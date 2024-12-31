d030ea6e337bf294dba66f2fc9492dfb0c8654cb
Tomer <tomermi123@gmail.com>
2024-12-31 14:10:46 +0200

    feat: add AnimatedLetters component and integrate into Home page; enhance styles for animations and layout

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,44 @@
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import './index.scss';
+import LogoT from '../../assets/logo/t-capital-png-no-bg.png';
+import AnimatedLetters from '../AnimatedLetters';
+
+const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+    const nameArray = ['o', 'm', 'e', 'r', ' ', 'M', 'i', 'z', 'r', 'a', 'h', 'i'];
+    const jobArray = ['a',' ', 'S', 'o', 'f', 't', 'w', 'a', 'r', 'e', ' ', 'E', 'n', 'g', 'i', 'n', 'e', 'e', 'r'];
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+          setLetterClass('text-animate-hover')
+        }, 4000);
+        return () => clearTimeout(timeoutId);
+      }, [])
+
+    return (
+        <>
+            <div className='container home-page'>
+                <div className='text-zone'>
+                    <h1>
+                        <span className={letterClass}>H</span>
+                        <span className={`${letterClass} _12`}>i,</span>
+                        <br />
+                        <span className={`${letterClass} _13`}>I</span>
+                        <span className={`${letterClass} _14`}>'m</span>
+                        <img className='logo' src={LogoT} alt='Software Engineer' />
+                        <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={8} />
+                        <br />
+                        <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={nameArray.length} />
+
+                    </h1>
+                    <h2>Full Stack | Backend | Frontend Developer</h2>
+                    <Link to="/contact" className="flat-button">CONTACT ME</Link>
+                </div>
+
+            </div>
+        </>
+    );
+}
+
+export default Home;
