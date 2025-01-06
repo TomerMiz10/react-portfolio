a0cb777b49ac603db0844c8d9d04e90867dac0c0
Tomer <tomermi123@gmail.com>
2025-01-06 21:13:42 +0200

    feat: update dependencies; add make.sh script for easier management; include new logo asset

---
+++ make.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+chmod +x "$0"
+
+# Define functions for installing dependencies, starting, stopping, and reloading the Docker containers, and viewing logs
+function install() {
+    npm install
+}
+
+function update() {
+    npm update
+}
+
+function pullOrigin() {
+    git pull origin master
+}
+
+function build {
+    docker-compose build
+}
+
+function force-build {
+    docker-compose up -d --force-recreate --renew-anon-volumes
+}
+
+function up() {
+    build
+    docker-compose run --rm --entrypoint "npm install" react-portfolio
+
+    docker-compose up -d
+}
+
+function down() {
+    docker-compose down
+}
+
+function reload() {
+    down
+    up
+}
+function logs() {
+    docker-compose logs -f
+}
+
+# Check the first argument to the script
+case $1 in
+    install)
+        install
+        ;;
+    update)
+        update
+        ;;
+    pullOrigin)
+        pullOrigin
+        ;;
+    up)
+        up
+        ;;
+    down)
+        down
+        ;;
+    reload)
+        reload
+        ;;
+    logs)
+        logs
+        ;;
+    build)
+        build
+        ;;
+    force-build)
+        force-build
+        ;;
+    *)
+        echo "Usage: ./make.sh {install|up|down|reload|logs}"
+        exit 1
+esac
