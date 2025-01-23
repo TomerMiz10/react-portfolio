f1e527d429a7e6cbbbcbd6e6ee15e1ecb411b8c4
Tomer <tomermi123@gmail.com>
2025-01-23 15:15:06 +0200

    Remove: Delete html and h1 styling tags in the titles of each page.

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -7,23 +7,7 @@ const Layout = () => {
         <div className='App'>
             <Sidebar />
             <div className='page'>
-
-                <span className='tags top-tags'>
-                    &lt;html&gt;
-                    <br />
-                    <span className='top-tag-body'>
-                        &lt;body&gt;
-                    </span>
-                </span>
-
                 <Outlet />
-                <span className='tags bottom-tags'>
-                    &lt;/body&gt;
-                    <br />
-                    <span className='bottom-tag-html'>
-                        &lt;/html&gt;
-                    </span>
-                </span>
             </div>
         </div>
     )
