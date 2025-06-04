83836e7bbe086325f55cfb07415761f3e7463ff1
Tomer <tomermi123@gmail.com>
2025-06-04 19:44:56 +0300

    refactor: render all sections in App.tsx for single-page layout

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,15 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
 import App from "./App.tsx"
-import { HashRouter } from "react-router-dom"
 import ScrollProvider from "@context/ScrollProvider"
 import ThemeModeProvider from "@context/ThemeModeProvider"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ScrollProvider>
-      <HashRouter>
-        <ThemeModeProvider>
-          <App />
-        </ThemeModeProvider>
-      </HashRouter>
+      <ThemeModeProvider>
+        <App />
+      </ThemeModeProvider>
     </ScrollProvider>
   </StrictMode>
 )
